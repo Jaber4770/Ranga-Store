fc4a20a9a60ad725b62680bf27d65a8849eb225b
jaber4770 <jaber.ahmed4770@gmail.com>
2021-09-13 17:42:52 +0600

    fixing total

--- js/app.js
+++ js/app.js
@@ -31,7 +31,6 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTotal();
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
@@ -39,7 +38,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -48,12 +47,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total * 100) / 100;
+  document.getElementById(id).innerText = total;
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value * 100) / 100;
+  document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
