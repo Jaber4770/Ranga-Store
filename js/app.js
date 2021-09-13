781e82b1be3ff645656eb33b87658c6aa8d2ffe7
jaber4770 <jaber.ahmed4770@gmail.com>
2021-09-13 16:33:16 +0600

    search button complate

--- js/app.js
+++ js/app.js
@@ -12,13 +12,15 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product ">
+    div.innerHTML = `<div class="single-product">
       <div>
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h3 class="text-info">${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
+      <p class="text-success">Rating: ${product.rating.rate} </p> 
+      <p class="text-success">Total review count: ${product.rating.count}</p>
+      <h2 class="text-primary">Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-info">Details</button></div>
       `;
